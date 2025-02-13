c085491f37e89a834ffaa396a891cad3c6d36469
Ranier Tan <rdavee0412@gmail.com>
2025-02-13 21:10:47 +0800

    feat: add document fee based on selected document type

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const authRoute = require("./routes/authRoutes");
 const userRoute = require("./routes/userRoute");
 const requestRoute = require("./routes/requestRoute");
 const trackerRoute = require("./routes/trackerRoute");
+const documentRoute = require("./routes/documentRoute");
 
 const app = express();
 const server = http.createServer(app);
@@ -25,6 +26,7 @@ app.use("/auth", authRoute);
 app.use("/api", userRoute);
 app.use("/request", requestRoute);
 app.use("/tracker", trackerRoute);
+app.use("/documents", documentRoute);
 
 server.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
